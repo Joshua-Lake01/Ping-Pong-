67c2e6d0d4efd10c40c8331ecc1e1182d0aabf20
Joshua Lake <110567609+Joshua-Lake01@users.noreply.github.com>
2022-09-29 19:58:34 +0100

    Add files via upload

---
+++ Ping Pong/Form1.Designer.vb
@@ -0,0 +1,322 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.LeftpaddleUp = New System.Windows.Forms.Timer(Me.components)
+        Me.LeftpaddleDown = New System.Windows.Forms.Timer(Me.components)
+        Me.RightPaddleUp = New System.Windows.Forms.Timer(Me.components)
+        Me.RightPaddleDown = New System.Windows.Forms.Timer(Me.components)
+        Me.Ballmovement = New System.Windows.Forms.Timer(Me.components)
+        Me.leftpaddle = New System.Windows.Forms.PictureBox()
+        Me.rightpaddle = New System.Windows.Forms.PictureBox()
+        Me.ball = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.ColorDialog1 = New System.Windows.Forms.ColorDialog()
+        Me.Label13 = New System.Windows.Forms.Label()
+        CType(Me.leftpaddle, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.rightpaddle, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ball, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'LeftpaddleUp
+        '
+        Me.LeftpaddleUp.Interval = 1
+        '
+        'LeftpaddleDown
+        '
+        Me.LeftpaddleDown.Interval = 1
+        '
+        'RightPaddleUp
+        '
+        Me.RightPaddleUp.Enabled = True
+        Me.RightPaddleUp.Interval = 5
+        '
+        'RightPaddleDown
+        '
+        Me.RightPaddleDown.Interval = 5
+        '
+        'Ballmovement
+        '
+        Me.Ballmovement.Enabled = True
+        Me.Ballmovement.Interval = 7
+        '
+        'leftpaddle
+        '
+        Me.leftpaddle.BackColor = System.Drawing.SystemColors.Control
+        Me.leftpaddle.Location = New System.Drawing.Point(12, 264)
+        Me.leftpaddle.Name = "leftpaddle"
+        Me.leftpaddle.Size = New System.Drawing.Size(44, 165)
+        Me.leftpaddle.TabIndex = 3
+        Me.leftpaddle.TabStop = False
+        '
+        'rightpaddle
+        '
+        Me.rightpaddle.BackColor = System.Drawing.SystemColors.Control
+        Me.rightpaddle.Location = New System.Drawing.Point(1119, 264)
+        Me.rightpaddle.Name = "rightpaddle"
+        Me.rightpaddle.Size = New System.Drawing.Size(44, 165)
+        Me.rightpaddle.TabIndex = 4
+        Me.rightpaddle.TabStop = False
+        '
+        'ball
+        '
+        Me.ball.BackColor = System.Drawing.SystemColors.Control
+        Me.ball.Location = New System.Drawing.Point(562, 331)
+        Me.ball.Name = "ball"
+        Me.ball.Size = New System.Drawing.Size(29, 31)
+        Me.ball.TabIndex = 5
+        Me.ball.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.125!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Lime
+        Me.Label1.Location = New System.Drawing.Point(1117, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(46, 51)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "0"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.125!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Lime
+        Me.Label2.Location = New System.Drawing.Point(12, 19)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(46, 51)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "0"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.Lime
+        Me.Label3.Location = New System.Drawing.Point(374, 100)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(397, 73)
+        Me.Label3.TabIndex = 13
+        Me.Label3.Text = "MAIN MENU"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.875!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.Lime
+        Me.Label4.Location = New System.Drawing.Point(481, 249)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(178, 42)
+        Me.Label4.TabIndex = 14
+        Me.Label4.Text = "RESUME"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.875!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.Lime
+        Me.Label5.Location = New System.Drawing.Point(517, 496)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(106, 42)
+        Me.Label5.TabIndex = 15
+        Me.Label5.Text = "QUIT"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.875!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.ForeColor = System.Drawing.Color.Lime
+        Me.Label6.Location = New System.Drawing.Point(495, 387)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(153, 42)
+        Me.Label6.TabIndex = 16
+        Me.Label6.Text = "Settings"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.875!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.ForeColor = System.Drawing.Color.Lime
+        Me.Label7.Location = New System.Drawing.Point(495, 249)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(148, 42)
+        Me.Label7.TabIndex = 17
+        Me.Label7.Text = "Dificulty"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.875!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.ForeColor = System.Drawing.Color.Lime
+        Me.Label8.Location = New System.Drawing.Point(506, 387)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(128, 42)
+        Me.Label8.TabIndex = 18
+        Me.Label8.Text = "Colour"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.875!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.ForeColor = System.Drawing.Color.Lime
+        Me.Label9.Location = New System.Drawing.Point(526, 496)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(80, 42)
+        Me.Label9.TabIndex = 19
+        Me.Label9.Text = "Exit"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.ForeColor = System.Drawing.Color.Lime
+        Me.Label10.Location = New System.Drawing.Point(392, 100)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(353, 73)
+        Me.Label10.TabIndex = 20
+        Me.Label10.Text = "SETTINGS"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Label11.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.875!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label11.ForeColor = System.Drawing.Color.Lime
+        Me.Label11.Location = New System.Drawing.Point(517, 301)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(40, 42)
+        Me.Label11.TabIndex = 21
+        Me.Label11.Text = "<"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Label12.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.875!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label12.ForeColor = System.Drawing.Color.Lime
+        Me.Label12.Location = New System.Drawing.Point(583, 301)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(40, 42)
+        Me.Label12.TabIndex = 22
+        Me.Label12.Text = ">"
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Label15.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.875!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label15.ForeColor = System.Drawing.Color.Lime
+        Me.Label15.Location = New System.Drawing.Point(552, 301)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(39, 42)
+        Me.Label15.TabIndex = 25
+        Me.Label15.Text = "0"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Label13.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.875!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label13.ForeColor = System.Drawing.Color.Lime
+        Me.Label13.Location = New System.Drawing.Point(12, 688)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(112, 42)
+        Me.Label13.TabIndex = 26
+        Me.Label13.Text = "Menu"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.ClientSize = New System.Drawing.Size(1198, 739)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label15)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ball)
+        Me.Controls.Add(Me.rightpaddle)
+        Me.Controls.Add(Me.leftpaddle)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.leftpaddle, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.rightpaddle, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ball, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents LeftpaddleUp As Timer
+    Friend WithEvents LeftpaddleDown As Timer
+    Friend WithEvents RightPaddleUp As Timer
+    Friend WithEvents RightPaddleDown As Timer
+    Friend WithEvents Ballmovement As Timer
+    Friend WithEvents leftpaddle As PictureBox
+    Friend WithEvents rightpaddle As PictureBox
+    Friend WithEvents ball As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label15 As Label
+    Friend WithEvents ColorDialog1 As ColorDialog
+    Friend WithEvents Label13 As Label
+End Class
---
+++ Ping Pong/Form1.vb
@@ -0,0 +1,336 @@
+﻿Public Class Form1
+    Dim counter1 As Integer
+    Dim counter2 As Integer
+    Dim ballupint As Decimal
+    Dim ballleftint As Decimal
+    Dim fheight As Integer
+    Dim rpaddleheight As Integer
+    Dim rpaddletop As Integer
+    Dim r1 As Integer
+    Dim r2 As Integer
+    Dim c1 As Integer
+
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label15.Text = 5
+        c1 = 5
+        Label3.Visible = False
+        Label4.Visible = False
+        Label5.Visible = False
+        Label6.Visible = False
+        Label7.Visible = False
+        Label8.Visible = False
+        Label9.Visible = False
+        Label10.Visible = False
+        Label11.Visible = False
+        Label12.Visible = False
+        Label15.Visible = False
+        counter1 = 0
+        counter2 = 0
+
+
+        ballupint = -3
+        ballleftint = 3
+
+    End Sub
+
+    Private Sub Form1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles Me.KeyPress
+        If e.KeyChar = "w" Then
+            LeftpaddleUp.Enabled = True
+
+        End If
+        If e.KeyChar = "s" Then
+            LeftpaddleDown.Enabled = True
+        End If
+
+
+
+
+
+    End Sub
+
+    Private Sub LeftpaddleUp_Tick(sender As Object, e As EventArgs) Handles LeftpaddleUp.Tick
+        If leftpaddle.Top <= 0 Then
+            LeftpaddleUp.Enabled = False
+        Else
+            leftpaddle.Top = leftpaddle.Top - 9
+            LeftpaddleUp.Enabled = False
+        End If
+    End Sub
+
+    Private Sub LeftpaddleDown_Tick(sender As Object, e As EventArgs) Handles LeftpaddleDown.Tick
+        If leftpaddle.Top >= Me.Height - 125 Then
+            LeftpaddleDown.Enabled = False
+        Else
+            leftpaddle.Top = leftpaddle.Top + 9
+            LeftpaddleDown.Enabled = False
+        End If
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles rightpaddle.Click
+
+    End Sub
+
+    Private Sub RightPaddleUp_Tick(sender As Object, e As EventArgs) Handles RightPaddleUp.Tick
+        Dim rpaddlecorrection As Integer
+        Dim rnd1 As Decimal
+        Dim ballhit As Boolean
+
+        rpaddlecorrection = rightpaddle.Top
+        rpaddlecorrection = rpaddlecorrection - 40
+        If c1 >= 5 Then
+            If rightpaddle.Top <= 0 Then
+                RightPaddleUp.Enabled = False
+                RightPaddleDown.Enabled = True
+            Else
+
+                rpaddlecorrection = ball.Top - 4
+                rightpaddle.Top = rpaddlecorrection
+
+            End If
+        End If
+        'If c1 < 5 Then
+        '    If rightpaddle.Top <= 0 Then
+        '        RightPaddleUp.Enabled = False
+        '        RightPaddleDown.Enabled = True
+        '    Else
+        '        rightpaddle.Top = rightpaddle.Top - 4
+
+        '    End If
+
+
+        'If c1 < 5 Then
+        '    If rightpaddle.Top <= 0 Then
+        '        RightPaddleUp.Enabled = False
+        '        RightPaddleDown.Enabled = True
+        '    Else
+        '        If rnd1 > 0.5 Then
+        '            rightpaddle.Top = rightpaddle.Top - 4
+        '        Else
+        '            rpaddlecorrection = ball.Top - 4
+        '            rightpaddle.Top = rpaddlecorrection
+        '        End If
+        '    End If
+        'End If
+
+        If c1 < 5 Then
+            If rightpaddle.Top <= 0 Then
+                RightPaddleUp.Enabled = False
+                RightPaddleDown.Enabled = True
+
+            Else
+
+                If ball.Top = rightpaddle.Top Then
+                    ballhit = True
+                    Randomize()
+                    rnd1 = Rnd()
+                    If rnd1 > 0.75 Then
+                        rightpaddle.Top = rightpaddle.Top - 4
+
+                    Else
+                        rpaddlecorrection = ball.Top - 4
+                        rightpaddle.Top = rpaddlecorrection
+                    End If
+                Else
+                    ballhit = False
+                End If
+
+                If ballhit = False Then
+
+                    rightpaddle.Top = rightpaddle.Top - 4
+
+                End If
+
+
+            End If
+        End If
+
+
+    End Sub
+
+    Private Sub RightPaddleDown_Tick(sender As Object, e As EventArgs) Handles RightPaddleDown.Tick
+        If c1 >= 5 Then
+            If rightpaddle.Top >= Me.Height - 125 Then
+                RightPaddleDown.Enabled = False
+                RightPaddleUp.Enabled = True
+            Else
+                rightpaddle.Top = ball.Top + 4
+
+            End If
+        End If
+        If c1 < 5 Then
+            If rightpaddle.Top >= Me.Height - 125 Then
+                RightPaddleDown.Enabled = False
+                RightPaddleUp.Enabled = True
+            Else
+                rightpaddle.Top = rightpaddle.Top + 4
+
+            End If
+        End If
+
+    End Sub
+
+    Private Sub Ballmovement_Tick(sender As Object, e As EventArgs) Handles Ballmovement.Tick
+
+
+        If ball.Top <= 1 Then
+            ballupint = -1 * ballupint
+        End If
+
+        If ball.Top >= Me.Height - 53 Then
+            ballupint = -1 * ballupint
+        End If
+
+        If ball.Left <= 0 Then
+            counter1 = counter1 + 1
+            Label1.Text = counter1
+            ball.Top = Me.Height * 0.5
+            ball.Left = Me.Width * 0.5
+        End If
+        If ball.Left >= Me.Width - 30 Then
+            counter2 = counter2 + 1
+            Label2.Text = counter2
+            ball.Top = Me.Height * 0.5
+            ball.Left = Me.Width * 0.5
+        End If
+
+        fheight = Me.Height
+        rpaddleheight = rightpaddle.Height
+        rpaddletop = rightpaddle.Top
+
+        If ball.Top > rpaddletop - 8 Then
+            If ball.Top < rpaddletop + rpaddleheight + 8 Then
+                If ball.Left + 10 >= rightpaddle.Left Then
+                    ballleftint = -1 * ballleftint
+                End If
+            End If
+        End If
+
+        If ball.Top > leftpaddle.Top - 8 Then
+            If ball.Top < leftpaddle.Top + leftpaddle.Height + 8 Then
+                If ball.Left < leftpaddle.Width + leftpaddle.Left Then
+
+                    ballleftint = -1 * ballleftint
+                End If
+            End If
+        End If
+
+        ball.Left = ball.Left + ballleftint
+        ball.Top = ball.Top + ballupint
+
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs)
+        Me.Close()
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs)
+        ballupint = +1
+        ballleftint = -1
+
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs)
+        ballupint = -1
+        ballleftint = +1
+
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+        Ballmovement.Enabled = True
+        RightPaddleDown.Enabled = True
+
+        Label3.Visible = False
+        Label4.Visible = False
+        Label5.Visible = False
+        Label6.Visible = False
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
+        Me.Close()
+
+    End Sub
+
+    Private Sub Label6_Click(sender As Object, e As EventArgs) Handles Label6.Click
+        Label3.Visible = False
+        Label4.Visible = False
+        Label5.Visible = False
+        Label6.Visible = False
+        Label7.Visible = True
+        Label8.Visible = True
+        Label9.Visible = True
+        Label10.Visible = True
+        Label11.Visible = True
+        Label12.Visible = True
+
+        Label15.Visible = True
+    End Sub
+
+    Private Sub Label9_Click(sender As Object, e As EventArgs) Handles Label9.Click
+        Label3.Visible = True
+        Label4.Visible = True
+        Label5.Visible = True
+        Label6.Visible = True
+        Label7.Visible = False
+        Label8.Visible = False
+        Label9.Visible = False
+        Label10.Visible = False
+        Label11.Visible = False
+        Label12.Visible = False
+
+        Label15.Visible = False
+
+    End Sub
+
+    Private Sub Label11_Click(sender As Object, e As EventArgs) Handles Label11.Click
+        If c1 = 0 Then
+
+        Else
+            c1 = c1 - 1
+            ballupint = ballupint * 0.75
+            ballleftint = ballleftint * 0.75
+        End If
+
+        Label15.Text = c1
+    End Sub
+
+    Private Sub Label12_Click(sender As Object, e As EventArgs) Handles Label12.Click
+
+        c1 = c1 + 1
+        ballupint = ballupint * 1.25
+        ballleftint = ballleftint * 1.25
+        Label15.Text = c1
+
+    End Sub
+
+    Private Sub Label14_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label8_Click(sender As Object, e As EventArgs) Handles Label8.Click
+        ColorDialog1.ShowDialog()
+        Me.BackColor = ColorDialog1.Color
+    End Sub
+
+    Private Sub Label13_Click(sender As Object, e As EventArgs) Handles Label13.Click
+        Label3.Visible = True
+        Label4.Visible = True
+        Label5.Visible = True
+        Label6.Visible = True
+        Ballmovement.Enabled = False
+        RightPaddleDown.Enabled = False
+        RightPaddleUp.Enabled = False
+    End Sub
+End Class
+
